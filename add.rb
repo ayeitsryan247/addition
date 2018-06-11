f4a4f9bc3f374030ff845e1a42f2a013021d905c
ayeitsryan247 <39201054+ayeitsryan247@users.noreply.github.com>
2018-06-11 11:03:05 -0400

    Addition

---
+++ add.rb
@@ -0,0 +1,12 @@
+#Welcoming Text
+puts "Enter any 2 numbers for the shell to add them."
+#Welcoming Text
+
+#Space
+puts "    "
+puts "Enter the first number"
+first = gets.chomp
+puts "Enter the second number"
+second = gets.chomp
+answer = first.to_i + second.to_i
+puts "The Answer is #{first} + #{second} = #{answer}"
