c956a136bd19b5a19c87d0877c7d95cc921c8fbb
Ryan Ward <39201054+ayeitsryan247@users.noreply.github.com>
2018-06-11 15:33:41 -0400

    Update add.rb

--- add.rb
+++ add.rb
@@ -1,12 +1,49 @@
+require 'colorize'
+
+def add()
+ 
 #Welcoming Text
 puts "Enter any 2 numbers for the shell to add them."
 #Welcoming Text
 
 #Space
 puts "    "
+
+
+#First Var input
 puts "Enter the first number"
-first = gets.chomp
-puts "Enter the second number"
-second = gets.chomp
-answer = first.to_i + second.to_i
-puts "The Answer is #{first} + #{second} = #{answer}"
+first = gets.chomp.to_i
+
+testVal = "true"
+while testVal == "true" do
+	#method selection
+	puts "Enter method : x,+"
+	wombat = gets.chomp.to_s
+
+	if wombat == "x" 
+		puts wombat
+		puts "Enter the second number"
+    second = gets.chomp.to_i
+
+    #Answer Output
+		if wombat == 'x'
+			answer = first * second
+		else
+			answer = first + second
+		end
+
+		puts "The Answer is #{first} #{wombat} #{second} = #{answer}".blue
+        exit
+
+	else
+		testVal = "false"
+		print testval
+		puts "Invalid Operator"
+		exit
+end
+end
+
+
+
+end
+add() # Calls adding Func
